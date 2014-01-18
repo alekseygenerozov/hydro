Fixed bug in the get_spatial_deriv function, which affected derivatives calculated on log grid. Grid index was improperly reused as loop index

hydro.py
@@ -278,8 +278,8 @@ class Grid:
 		#Getting stencil for current grid point
 		stencil=self._get_stencil(i, left=left, right=right)
 		field_list=np.zeros(num_zones)
-		for i in range(num_zones):
-			field_list[i]=getattr(stencil[i], field)
+		for j in range(num_zones):
+			field_list[j]=getattr(stencil[j], field)
 
 		#Coefficients we will use.
 		coeffs=np.array([-1., 9., -45., 0., 45., -9., 1.])/60.
@@ -374,8 +374,8 @@ class Grid:
 		lap_vel=self.get_spatial_deriv(i, 'vel', 'second')
 		art_visc=min(self.grid[i].cs,  np.abs(self.grid[i].vel))*(self.radii[self.length-1]-self.radii[0])/self.Re
 
-		if np.abs(rad-7.51E11)/rad<0.01:
-			import pdb; pdb.set_trace();
+		# if np.abs(rad-7.51E11)/rad<0.01:
+		# 	import pdb; pdb.set_trace();
 
 		#Need to be able to handle for general potential in the future
 		return -vel*dv_dr-dlog_rho_dr*(kb*temp/mp)+(kb/mp)*dtemp_dr-(G*self.M)/rad**2+art_visc*lap_vel-(self.q(rad)*vel/rho)

parker.py
@@ -26,7 +26,7 @@ c_s=np.sqrt(kb*temp/mp)
 rc=G*m*M_sun/(2*c_s**2)
 
 rmin=5.E11
-rmax=5.E12
+rmax=1.E12
 
 
 ##Run a command from the bash shell
@@ -106,7 +106,7 @@ def background(rad, rho_0=0.9*floor, temp=temp, log=True):
     
 
 def deriv_test(rad, log=True):
-    return [np.log(2*floor), rad**2, 1.]    
+    return [np.log(2*floor), np.log(rad), 1.]    
 
 
 
@@ -119,7 +119,7 @@ d=dict(log=True)
 #     q=power_src, symbol='r', logr=False)
 # grid.evolve(50*tcross,analytic_func=[None, None, None, None, None, None])
 grid=hydro.Grid(rmin, rmax, parker, M=M_sun, n=100, safety=0.6, Re=150., params=d, symbol='r', logr=True, floor=floor)
-grid.evolve(3*tcross,analytic_func=[None, None, None, None, None, None], max_steps=0)
+grid.evolve(3*tcross,analytic_func=[None, None, None, None, None, None])
 
 
 #bash_command('cp '+'tmp tmp_log')