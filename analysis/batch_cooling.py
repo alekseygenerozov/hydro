9c4a51ea1bdf3fe42e10fda1c452c51e6dc51ae8
Aleksey <aleksey.generozov@gmail.com>
2014-07-11 15:04:43 -0400

    Adding more analysis routines.

--- analysis/batch_cooling.py
+++ analysis/batch_cooling.py
@@ -7,8 +7,6 @@ import galaxy
 import shlex
 import numpy as np
  
-
-
 import matplotlib as mpl
 import matplotlib.pylab as plt
 import pickle
@@ -31,69 +29,65 @@ c=const.c.cgs.value
 
 gal_dict=galaxy.nuker_params()
 fig,ax=plt.subplots(1, sharex=True, figsize=(10, 8))
-ax.set_xlabel('$v_w/\sigma$')
-ax.set_ylabel(r'$r_{\rm stag}/r_{\rm soi}$')
+
 linestyles=['-.', '-']
 vws=[200., 500., 1000.]
 # selection=['NGC3115', 'NGC1172', 'NGC4478']
 cols=brewer2mpl.get_map('Set2', 'qualitative', 3).mpl_colors
-
 
 for name in gal_dict.keys():
 	base_d='/Users/aleksey/Second_Year_Project/hydro/batch/'+name
 	gal_data='/Users/aleksey/Second_Year_Project/hydro/gal_data/'+name
 	for j,vw in enumerate(vws):
 		d=base_d+'/vw_'+str(vw)
+		if not sc.check(d):
+			continue
 		try:
 			saved=np.load(d+'/save.npz')['a']
 		except:
 			continue
-		if not sc.check(d):
-			continue
-
-		start=galaxy.prepare_start(saved[-1])
-		gal=galaxy.NukerGalaxy(name, gal_dict, init_array=start)
-		if gal.params['type']=='Core':
-			symbol='<'
-		else:
-			symbol='s'
-
 		try:
 			rsoi=np.genfromtxt(gal_data+'/rsoi')
 			sigma=np.genfromtxt(gal_data+'/sigma')
 		except:
 			continue
-		sigma_interp=interp1d(sigma[:,0], sigma[:,1])
 
-		ax.loglog(vw*1.E5/sigma_interp(rsoi*pc), gal.rs/pc/rsoi, symbol, color=cols[j], markersize=10)
+		saved=np.load(d+'/save.npz')['a']
+		start=galaxy.prepare_start(saved[-1])
+		gal=galaxy.NukerGalaxy(name, gal_dict, init_array=start)
+		
+		heating=gal.q_grid*(0.5*gal.vel**2+0.5*(vw*1.E5)**2+0.5*sigma[:,1]**2)
+		cooling=gal.cooling
+
+		np.savetxt(d+'/hc_ratio', np.transpose([gal.radii, heating/cooling]))
+
 
 for name in gal_dict.keys():
 	base_d='/Users/aleksey/Second_Year_Project/hydro/batch_A2052/'+name
 	gal_data='/Users/aleksey/Second_Year_Project/hydro/gal_data/'+name
 	for j,vw in enumerate(vws):
 		d=base_d+'/vw_'+str(vw)
+		if not sc.check(d):
+			continue
 		try:
 			saved=np.load(d+'/save.npz')['a']
 		except:
 			continue
-		if not sc.check(d):
-			continue
-
-		start=galaxy.prepare_start(saved[-1])
-		gal=galaxy.NukerGalaxy(name, gal_dict, init_array=start)
-		if gal.params['type']=='Core':
-			symbol='<'
-		else:
-			symbol='s'
-
 		try:
 			rsoi=np.genfromtxt(gal_data+'/rsoi')
 			sigma=np.genfromtxt(gal_data+'/sigma')
 		except:
 			continue
-		sigma_interp=interp1d(sigma[:,0], sigma[:,1])
 
-		ax.loglog(vw*1.E5/sigma_interp(rsoi*pc), gal.rs/pc/rsoi, symbol, color=cols[j], markersize=10)
+		saved=np.load(d+'/save.npz')['a']
+		start=galaxy.prepare_start(saved[-1])
+
+		gal=galaxy.NukerGalaxy(name, gal_dict, init_array=start)
+		heating=gal.q_grid*(0.5*gal.vel**2+0.5*(vw*1.E5)**2+0.5*sigma[:,1]**2)
+		cooling=gal.cooling
+
+		np.savetxt(d+'/hc_ratio', np.transpose([gal.radii, heating/cooling]))
 
 
-plt.savefig('rs.png')
+	
+
