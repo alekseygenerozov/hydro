7794510bd18685ccda9714501e72b95a7704aa24
Aleksey <aleksey.generozov@gmail.com>
2014-07-14 18:03:06 -0400

    Adding script to compute profile for NGC4486

---
+++ scripts/NGC4486.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import galaxy 
+import numpy as np
+
+gal_name='NGC4486'
+save='/Users/aleksey/Second_Year_Project/hydro/batch_A2052/vw_1000.0'+gal_name
+saved=np.load(save+'/save.npz')['a']
+saved=saved[-1]
+
+vw=5.E7
+outdir=''
+#Set up galaxy for run
+start=galaxy.prepare_start(saved)
+gal_dict=galaxy.nuker_params()
+gal=galaxy.NukerGalaxy(gal_name, gal_dict, init_array=start)
+
+if outdir:
+	gal.set_param('outdir', outdir)
+else:
+	gal.set_param('outdir', gal_name+'/vw_'+str(vw/1.E5))
+
+gal.set_param('vw_extra',)
+gal.set_param('Re_s', 500.)
+gal.set_param('visc_scheme', 'cap_visc')
+
+gal.solve(5.*gal.tcross)
