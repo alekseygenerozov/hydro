1e5e2a0f692d0d7dfd9811d9849cbcf899433eee
Aleksey <aleksey.generozov@gmail.com>
2014-07-14 19:30:03 -0400

    Modifying NGC4486 script.

--- scripts/NGC4486.py
+++ scripts/NGC4486.py
@@ -4,7 +4,7 @@ import galaxy
 import numpy as np
 
 gal_name='NGC4486'
-save='/Users/aleksey/Second_Year_Project/hydro/batch_A2052/vw_1000.0'+gal_name
+save='/Users/aleksey/Second_Year_Project/hydro/batch_A2052/'+gal_name+'/vw_1000.0'
 saved=np.load(save+'/save.npz')['a']
 saved=saved[-1]
 
@@ -20,8 +20,8 @@ if outdir:
 else:
 	gal.set_param('outdir', gal_name+'/vw_'+str(vw/1.E5))
 
-gal.set_param('vw_extra',)
-gal.set_param('Re_s', 500.)
-gal.set_param('visc_scheme', 'cap_visc')
+gal.set_param('vw_extra',vw)
+# gal.set_param('Re_s', 500.)
+# gal.set_param('visc_scheme', 'cap_visc')
 
 gal.solve(5.*gal.tcross)
