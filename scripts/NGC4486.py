85429b21608a215be8e5a2ec521b70c8fbfc06a0
Aleksey <aleksey.generozov@gmail.com>
2014-07-17 15:52:27 -0400

    script NGC4486.py to do NGC4486 with vw=500 km/s and visc_scheme=cap_visc.

--- scripts/NGC4486.py
+++ scripts/NGC4486.py
@@ -3,27 +3,26 @@
 import galaxy 
 import numpy as np
 
-gal_name='NGC4486'
-save='/Users/aleksey/Second_Year_Project/hydro/batch_A2052/'+gal_name+'/vw_1000.0'
+gal_name='NGC4486_1'
+save='/vega/astro/users/ag3293/NGC4486_1/vw_500.0'
 saved=np.load(save+'/save.npz')['a']
 saved=saved[-1]
-
 
 outdir=''
 vw=5.E7
 #Set up galaxy for run
 start=galaxy.prepare_start(saved)
 gal_dict=galaxy.nuker_params()
-gal=galaxy.NukerGalaxy(gal_name, gal_dict, init_array=start)
+gal=galaxy.NukerGalaxy('NGC4486', gal_dict, init_array=start)
 
 if outdir:
 	gal.set_param('outdir', outdir)
 else:
 	gal.set_param('outdir', gal_name+'/vw_'+str(vw/1.E5))
 
-gal.set_param('vw_extra',1.E8)
-
+gal.set_param('vw_extra',vw)
 gal.set_param('Re_s', 500.)
-# gal.set_param('visc_scheme', 'cap_visc')
+gal.set_param('visc_scheme', 'cap_visc')
 
-gal.solve_adjust(5.*gal.tcross, 'vw_extra', 5.E7)
+gal.solve(5.*gal.tcross)
+gal.backup()
