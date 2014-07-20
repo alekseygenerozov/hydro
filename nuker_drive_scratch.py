ca658bb6ee7722f8df44cc5f376a0389d6b720ce
Aleksey <aleksey.generozov@gmail.com>
2014-07-20 12:59:19 -0400

    Adding new script to calculate NGC4551 from scratch. Added test of boundary condition implemementation.

---
+++ nuker_drive_scratch.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import galaxy
+import parker
+import sol_check as sc
+
+import astropy.constants as const
+import numpy as np
+
+import argparse
+
+import ipyani
+from math import e
+
+from astropy.io import ascii
+from astropy.table import Column
+
+
+
+#Constants
+G=const.G.cgs.value
+M_sun=const.M_sun.cgs.value
+kb=const.k_B .cgs.value
+mp=const.m_p.cgs.value
+h=const.h.cgs.value
+c=const.c.cgs.value
+pc=const.pc.cgs.value
+
+
+def main():
+	gal_dict=galaxy.nuker_params()
+	gal=galaxy.NukerGalaxy('NGC4551',gal_dict,init={
+		,'f_initial':galaxy.background,'func_params':{'log': True, 'temp': 1.E7, 'rho_0': 1e-23}, 'length':init[i]['length']}))
+
+	gal.isot_on()
+	gal.set_param('bdry', 'bp')
+	gal.set_param('eps', 0.)
+	gal.set_param('vw_extra', 1.E8)
+	gal.set_param('sigma_heating', False)
+
+	gal.solve(2.*gal.tcross)
+	gal.isot_off()
+	gal.solve(2.*gal.tcross)
+	#Resetting boundary conditions
+	gal.set_param('Re_s', 500.)
+	gal.set_param('bdry', 'default')
+	gal.solve(gal.tcross)
+	#Turning on the stellar potential
+	gal.set_param('eps',1.)
+	gal.solve(3.*gal.tcross)
+	gal.set_param('sigma_heating', True)	
+	gal.solve(8.*gal.tcross)
+
+	gal.backup()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
---
+++ tests/test_bdry.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import galaxy
+import parker 
+import sys
+import numpy as np
+
+# -----------------------------------------------------------------
+# This block of code tells Python to drop into the debugger
+# if there is an uncaught exception when run from the command line.
+def info(type, value, tb):
+	if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+	# we are in interactive mode or we don't have a tty-like
+	# device, so we call the default hook
+		sys.__excepthook__(type, value, tb)
+	else:
+		import traceback, pdb
+		# we are NOT in interactive mode, print the exception...
+		traceback.print_exception(type, value, tb)
+		print
+		# ...then start the debugger in post-mortem mode.
+		pdb.pm()
+sys.excepthook = info
+# -----------------------------------------------------------------
+
+
+
+def test_bdry_def():
+	start=np.load('data/power_laws.npz')['arr_0']
+	start_copy=np.copy(start)
+	gal=galaxy.Galaxy(init_array=start)
+
+	gal._update_ghosts()
+
+	assert np.allclose(gal.rho, np.exp(start_copy[:,1]))
+	assert np.allclose(gal.vel, start_copy[:,2])
+
+def test_bdry_bp():
+	start=np.load('data/bp.npz')['arr_0']
+	start_copy=np.copy(start)
+	gal=galaxy.Galaxy(init_array=start)
+	gal.set_param('bdry', 'bp')
+	assert gal.bdry=='bp'
+
+	gal._update_ghosts()
+	assert np.allclose(gal.rho, np.exp(start_copy[:,1]))
+	assert np.allclose(gal.vel, start_copy[:,2])
+	
+
+
+
+
