ba06e300cfdca7c42c701996b1625b81132208c2
Aleksey <aleksey.generozov@gmail.com>
2014-07-20 01:19:03 -0400

    Adding script for NGC3605.

---
+++ scripts/NGC3605.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import galaxy 
+import numpy as np
+
+gal_name='NGC3605'
+save='/vega/astro/users/ag3293/'+gal_name+'/vw_200.0'
+saved=np.load(save+'/save.npz')['a']
+saved=saved[-1]
+
+outdir=''
+vw=2.E7
+#Set up galaxy for run
+start=galaxy.prepare_start(saved)
+gal_dict=galaxy.nuker_params()
+gal=galaxy.NukerGalaxy(gal_name, gal_dict, init_array=start)
+
+if outdir:
+	gal.set_param('outdir', outdir)
+else:
+	gal.set_param('outdir', gal_name+'/vw_'+str(vw/1.E5))
+
+gal.set_param('vw_extra',vw)
+gal.solve(3.*gal.tcross)
+gal.set_param('visc_scheme', 'cap_visc')
+gal.solve(3.*gal.tcross)
+
+gal.backup()
