43c718d81c5cfc168f6da5f8ce673e2f08463c0e
Aleksey <aleksey.generozov@gmail.com>
2014-07-20 12:51:30 -0400

    Some modifications to NGC3605 script.

--- scripts/NGC3605.py
+++ scripts/NGC3605.py
@@ -4,7 +4,7 @@ import galaxy
 import numpy as np
 
 gal_name='NGC3605'
-save='/vega/astro/users/ag3293/'+gal_name+'/vw_200.0'
+save='/vega/astro/users/ag3293/'+gal_name+'_1/vw_200.0'
 saved=np.load(save+'/save.npz')['a']
 saved=saved[-1]
 
@@ -21,6 +21,7 @@ else:
 	gal.set_param('outdir', gal_name+'/vw_'+str(vw/1.E5))
 
 gal.set_param('vw_extra',vw)
+gal.set_param('Re_s', 1.E20)
 gal.solve(3.*gal.tcross)
 gal.set_param('visc_scheme', 'cap_visc')
 gal.solve(3.*gal.tcross)
