Modifying drive to be consistent with new style initialization. Also, fixed units issue in rs_analytic. Rescaling of initialization array also fixed.

galaxy.py
@@ -276,8 +276,9 @@ class Galaxy(object):
 		else:
 			init_array=np.load(loc+'/save.npz')['a']
 
+		init_array[:,0]=rescale*init_array[:,0]
 		radii=init_array[:,0]
-		radii=rescale*radii
+		
 		init['r1']=radii[0]
 		init['r2']=radii[-1]
 
@@ -1058,11 +1059,9 @@ class NukerGalaxy(Galaxy):
 		else:
 			init_array=np.load(loc+'/save.npz')['a']
 
-
-		init_array=prepare_start(np.load(loc+'/save.npz')['a'][index])
-
+		init_array[:,0]=rescale*init_array[:,0]
 		radii=init_array[:,0]
-		radii=rescale*radii
+
 		init['r1']=radii[0]
 		init['r2']=radii[-1]
 
@@ -1152,7 +1151,7 @@ class NukerGalaxy(Galaxy):
 		'''Analytic formula for the stagnation radius'''
 		A=(4.*self.gamma-(1+self.params['gamma'])*(self.gamma-1.))/(4.*(self.gamma-1.))
 		eta=self.vw_extra/self.sigma(self.rinf)
-		omega=self.M_enc(self.rs)/self.params['M']
+		omega=self.M_enc(self.rs/pc)/self.params['M']
 
 		lrho_interp=interp1d(np.log(self.radii),self.log_rho)
 		dens_slope=np.abs(derivative(lrho_interp, np.log(self.rs), dx=self.delta_log[0]))
@@ -1162,7 +1161,7 @@ class NukerGalaxy(Galaxy):
 	@property 
 	def rs_residual(self):
 		'''Residual of the stagnation radius from the analytic result'''
-		return (self.rs_analytic-(self.rs/self.rinf))/self.rs_analytic
+		return (self.rs_analytic-(self.rs/pc/self.rinf))/self.rs_analytic
 
 	def rho_func(self, r):
 		try:

nuker_drive.py
@@ -83,9 +83,7 @@ def main():
 	for i in range(len(init)):
 		gal_name=init[i]['gal']
 		try:
-			start=galaxy.prepare_start(np.load(init[i]['save']+'/save.npz')['a'][init[i]['index']], rescale=init[i]['rescale'])
-
-			gal=galaxy.NukerGalaxy(gal_name,gal_dict,init_array=start)
+			gal=galaxy.NukerGalaxy.from_dir(gal_name,init[i]['save'], rescale=rescale, index=index)
 			gal.set_param('vw_extra', init[i]['vw_extra'])
 		except:
 			continue