ea520e74587b55c53e7dae62ca50707aedb048cb
Aleksey <aleksey.generozov@gmail.com>
2014-08-03 16:38:53 +0100

    Adding params_parse which parses config file for driver.

---
+++ params_parse.py
@@ -0,0 +1,25 @@
+from ConfigParser import SafeConfigParser
+import ast
+
+class CaseConfigParser(SafeConfigParser):
+	def __init__(self):
+		SafeConfigParser.__init__(self)
+		self.optionxform = str
+
+def params_parse(conf_file):
+	config = CaseConfigParser()
+	params_dict={}
+	param_names=['Re','Re_s','visc_scheme', 'bdry']
+
+	try:
+		f=open(conf_file,'r')
+		config.readfp(f)
+	except: 
+		return {}
+	for name in param_names:
+		try:
+			params_dict[name]=ast.literal_eval(config.get('params',name))
+		except:
+			pass
+
+	return params_dict
