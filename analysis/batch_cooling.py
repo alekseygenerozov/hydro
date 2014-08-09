6b0eaba29977f9cee61dd200cb6d8df744aec67e
Aleksey <aleksey.generozov@gmail.com>
2014-08-09 18:13:08 -0400

    Changes to batch_cooling.py

--- analysis/batch_cooling.py
+++ analysis/batch_cooling.py
@@ -9,7 +9,7 @@ import numpy as np
  
 import matplotlib as mpl
 import matplotlib.pylab as plt
-import pickle
+import dill
 
 from scipy.interpolate import interp1d
 import brewer2mpl
@@ -36,58 +36,21 @@ vws=[200., 500., 1000.]
 cols=brewer2mpl.get_map('Set2', 'qualitative', 3).mpl_colors
 
 for name in gal_dict.keys():
-	base_d='/Users/aleksey/Second_Year_Project/hydro/batch/'+name
+	base_d='/Users/aleksey/Second_Year_Project/hydro/batch_collected/'+name
 	gal_data='/Users/aleksey/Second_Year_Project/hydro/gal_data/'+name
 	for j,vw in enumerate(vws):
 		d=base_d+'/vw_'+str(vw)
-		if not sc.check(d):
-			continue
-		try:
-			saved=np.load(d+'/save.npz')['a']
-		except:
-			continue
-		try:
-			rsoi=np.genfromtxt(gal_data+'/rsoi')
-			sigma=np.genfromtxt(gal_data+'/sigma')
-		except:
+		if sc.check(d):
 			continue
 
-		saved=np.load(d+'/save.npz')['a']
-		start=galaxy.prepare_start(saved[-1])
-		gal=galaxy.NukerGalaxy(name, gal_dict, init_array=start)
-		
-		heating=gal.q_grid*(0.5*gal.vel**2+0.5*(vw*1.E5)**2+0.5*sigma[:,1]**2)
+		dill.load(open('grid.p','rb'))
+
+		heating=gal.q_grid*(0.5*gal.vel**2+0.5*(vw*1.E5)**2+0.5*gal.sigma_grid**2)
 		cooling=gal.cooling
 
-		np.savetxt(d+'/hc_ratio', np.transpose([gal.radii, heating/cooling]))
+		ax.loglog(gal.radii, heating/cooling, color=cols[j], label=vw)
 
-
-for name in gal_dict.keys():
-	base_d='/Users/aleksey/Second_Year_Project/hydro/batch_A2052/'+name
-	gal_data='/Users/aleksey/Second_Year_Project/hydro/gal_data/'+name
-	for j,vw in enumerate(vws):
-		d=base_d+'/vw_'+str(vw)
-		if not sc.check(d):
-			continue
-		try:
-			saved=np.load(d+'/save.npz')['a']
-		except:
-			continue
-		try:
-			rsoi=np.genfromtxt(gal_data+'/rsoi')
-			sigma=np.genfromtxt(gal_data+'/sigma')
-		except:
-			continue
-
-		saved=np.load(d+'/save.npz')['a']
-		start=galaxy.prepare_start(saved[-1])
-
-		gal=galaxy.NukerGalaxy(name, gal_dict, init_array=start)
-		heating=gal.q_grid*(0.5*gal.vel**2+0.5*(vw*1.E5)**2+0.5*sigma[:,1]**2)
-		cooling=gal.cooling
-
-		np.savetxt(d+'/hc_ratio', np.transpose([gal.radii, heating/cooling]))
-
+fig.savefig('cooling.pdf')
 
 	
 
