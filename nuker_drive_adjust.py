f9a96aa499f879def4ea145db3c8f09ac59f6686
Aleksey <aleksey.generozov@gmail.com>
2014-09-23 17:14:51 -0400

    Some changes to the nuker_drive_adjust

--- nuker_drive_adjust.py
+++ nuker_drive_adjust.py
@@ -1,3 +1,6 @@
+
+
+
 #!/usr/bin/env python
 
 import galaxy
@@ -32,25 +35,32 @@ def main():
 	init=ascii.read(args.init[0])
 
 	for i in range(len(init)):
+		#Opening the specified pickled galaxy object. Will use this parameters of the pickled grid.
 		try:
 			print init[i]['pickle']
 			gal=dill.load(open(init[i]['pickle'], 'rb'))
 		except:
 			print 'Could not open pickle!'
 			continue
-		print init[i]['target']
 
+		#Useful name for the output dir.
 		try:
 			gal.set_param('outdir', init[i]['outdir'])
-		except:
+		except KeyError:
 			gal.set_param('outdir', gal.name+'/vw_{0}_{1}_{2}'.format(gal.vw_extra/1.E5, init[i]['param'], init[i]['target']))
 
+		#Behavior of code depends on the type of param that is passed, it is a float which may be gradually adjusted then 
+		#solve while gradually adjusting parameter, using the solve_adjust method.
 		if type(init[i]['target'])==float:
 			gal.solve_adjust(5.*gal.tcross, init[i]['param'], init[i]['target'])
 		else:
 			gal.set_param(init[i]['param'], init[i]['target'])
-		gal.solve()
-		gal.backup()
+
+		#If time is specified then run for that amount of time otherwise, run until conservation checks are specified 
+		try:
+			gal.solve(init[i]['time']*gal.tcross)
+		except KeyError:
+			gal.solve()
 
 		galaxy.bash_command('cp '+init[i]['pickle']+' '+gal.outdir+'/init.p')
 
